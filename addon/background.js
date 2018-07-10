e9d8fa735d2414321bbf763e684b0ff4bb134b30
Erik Rose <erik@mozilla.com>
2018-07-10 16:50:46 -0400

    Restore the original synchronous approach. All external messages work now.
    Turns out I was editing the wrong file when it wasn't working.

--- addon/background.js
+++ addon/background.js
@@ -1,6 +1,5 @@
 /** Handle messages that come in from the FathomFox webext. */
-async function handleExternalMessage(request, sender, sendResponse) {
-    console.log('got external message');
+function handleExternalMessage(request, sender, sendResponse) {
     if (request.type === 'rulesetSucceededOnTabs') {
         // Run a given ruleset on a given set of tabs, and return an array
         // of bools saying whether they got the right answer on each.
@@ -11,14 +10,11 @@ async function handleExternalMessage(request, sender, sendResponse) {
                  trainableId: request.trainableId,
                  coeffs: request.coeffs})));
     } else if (request.type === 'trainableKeys') {
-        console.log('got request for keys');  // To get this to fire, reload the FathomFox extension, then reopen the Training page.
-        console.log(request);
         // Return an array of IDs of rulesets we can train.
-        return 'response!'; //trainables.keys());
+        sendResponse(Array.from(trainables.keys()));
     } else if (request.type === 'trainableCoeffs') {
         // Return the initial coeffs of some ruleset.
-        return 'response again!'; //trainables.keys());
-        //sendResponse(trainables.get(request.trainableId).coeffs);
+        sendResponse(trainables.get(request.trainableId).coeffs);
     }
 }
 browser.runtime.onMessageExternal.addListener(handleExternalMessage);
