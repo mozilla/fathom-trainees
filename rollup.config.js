eab0c7f4a1511adcdd71bbf749516b247975590b
Erik Rose <erik@mozilla.com>
2018-07-10 16:14:16 -0400

    The external-message handler fires, but the sender just gets an empty object as a reply.

---
+++ rollup.config.js
@@ -0,0 +1,34 @@
+import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
+import json from 'rollup-plugin-json';
+import builtins from 'rollup-plugin-node-builtins';
+import globals from 'rollup-plugin-node-globals';
+
+/**
+ * Return typical rollup settings for a file of a given name.
+ */
+function mindlesslyFactoredOutSettings(name) {
+    return {
+        input: 'src/' + name + '.js',
+        output: {
+            file: 'addon/' + name + '.js',
+            format: 'iife',
+            name  // Convention: name the var the same thing.
+        },
+        plugins: [
+            resolve({preferBuiltins: true}),
+            commonjs({
+                namedExports: {
+                    'wu': ['forEach', 'map', 'flatten']
+                }
+            }),
+            json(),
+            globals(),
+            builtins()
+        ]
+    }
+}
+
+export default [
+    mindlesslyFactoredOutSettings('trainables')
+];
---
+++ src/background.js
@@ -0,0 +1,24 @@
+/** Handle messages that come in from the FathomFox webext. */
+function handleExternalMessage(request, sender, sendResponse) {
+    console.log('got external message');
+    if (request.type === 'rulesetSucceededOnTabs') {
+        // Run a given ruleset on a given set of tabs, and return an array
+        // of bools saying whether they got the right answer on each.
+        return Promise.all(request.tabIds.map(
+            tabId => browser.tabs.sendMessage(
+                tabId,
+                {type: 'rulesetSucceeded',
+                 trainableId: request.trainableId,
+                 coeffs: request.coeffs})));
+    } else if (request.type === 'trainableKeys') {
+        console.log('got request for keys');  // To get this to fire, reload the FathomFox extension, then reopen the Training page.
+        console.log(request);
+        // Return an array of IDs of rulesets we can train.
+        sendResponse('response!'); //trainables.keys());
+    } else if (request.type === 'trainableCoeffs') {
+        // Return the initial coeffs of some ruleset.
+        sendResponse('response again!'); //trainables.keys());
+        //sendResponse(trainables.get(request.trainableId).coeffs);
+    }
+}
+browser.runtime.onMessageExternal.addListener(handleExternalMessage);
