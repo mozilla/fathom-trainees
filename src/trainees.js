d79e00727361eb838919f3338564d98303b32a56
Douglas McDermott <douglas.mcdermott@mail.utoronto.ca>
2019-01-30 20:43:31 -0500

    Add files via upload
    Model to train close button detection. Currently at 20%

--- src/trainees.js
+++ src/trainees.js
@@ -15,135 +15,11 @@ import {ancestors} from 'fathom-web/utilsForFrontend';
 const trainees = new Map();
 
 trainees.set(
-    'popUp',
-    {coeffs: [1,1,2,3,1],  
-
-     rulesetMaker:
-        function ([coeffWidth, coeffNearlyOpaque, coeffForm, coeffClassOrId, coeffVisible]) {
-
-            const ZEROISH = .08;
-            const ONEISH = .9;
-
-            function oneThirdWidth(fnode) {
-                const rect = fnode.element.getBoundingClientRect();
-                const wDiff = rect.width / window.innerWidth;
-                return trapezoid(rect.width, window.innerWidth/3, window.innerWidth) ** coeffWidth;
-            }
-
-            // exponentially decrease each level's score multiplier
-            function containsForm(fnode) {
-                const element = fnode.element;
-                let queue = [element];
-                let formMultiplier = 1;
-
-                for (let i = 1; i < 5; i++){
-                    let nextQueue = [];
-                    while (queue.length > 0){
-                        let e = queue.pop();
-                        nextQueue = nextQueue.concat(Array.from(e.children));
-                        if (e.nodeName === "FORM"){
-                            formMultiplier = formMultiplier * (1+coeffForm/i);
-                        }
-                    }
-                    queue = nextQueue;
-                }
-                return formMultiplier;
-            }
-
-            function suspiciousClassOrId(fnode) {
-                const element = fnode.element;
-                const attributeNames = ['class', 'id'];
-                let numOccurences = 0;
-                function numberOfSuspiciousSubstrings(value) {
-                    return value.includes('popup') + value.includes('subscription') + value.includes('newsletter')+ value.includes('modal');
-                }
-
-                for (const name of attributeNames) {
-                    let values = element.getAttribute(name);
-                    if (values) {
-                        if (!Array.isArray(values)) {
-                            values = [values];
-                        }
-                        for (const value of values) {
-                            numOccurences += numberOfSuspiciousSubstrings(value);
-                        }
-                    }
-                }
-                const logisticFunction = logisticFuncGenerator(2,0,coeffClassOrId);
-
-                return logisticFunction(numOccurences);
-            }
-
-            function buttons(fnode) {
-                let descendants = Array.from(fnode.element.querySelectorAll("*"));
-                let buttonCounter = 0;
-                for (const d of descendants){
-                    if(d.nodeName === "INPUT") {
-                        buttonCounter += 1;
-                    }
-                }
-                return (buttonCounter > 4) ? 1 + 1/buttonCounter : buttonCounter;
-            }
-
-            
-
-            /* Utility procedures */
-
-            /**
-             * Scale a number to the range [ZEROISH, ONEISH].
-             *
-             * For a rising trapezoid, the result is ZEROISH until the input
-             * reaches zeroAt, then increases linearly until oneAt, at which it
-             * becomes ONEISH. To make a falling trapezoid, where the result is
-             * ONEISH to the left and ZEROISH to the right, use a zeroAt greater
-             * than oneAt.
-             */
-            function trapezoid(number, zeroAt, oneAt) {
-                const isRising = zeroAt < oneAt;
-                if (isRising) {
-                    if (number <= zeroAt) {
-                        return ZEROISH;
-                    } else if (number >= oneAt) {
-                        return ONEISH;
-                    }
-                } else {
-                    if (number >= zeroAt) {
-                        return ZEROISH;
-                    } else if (number <= oneAt) {
-                        return ONEISH;
-                    }
-                }
-                const slope = (ONEISH - ZEROISH) / (oneAt - zeroAt);
-                return slope * (number - zeroAt) + ZEROISH;
-            }
-
-            // returns a logistic function. Ideally we should input the growth rate as
-            // a hyperparameter from the optimization
-            function logisticFuncGenerator(maxVal, xMid, growthRate){
-                return x=> (maxVal)/(1+Math.exp(-1*growthRate*(x-xMid)));
-            }
-
-            /* The actual ruleset */
-
-            const rules = ruleset(
-                rule(dom('div'), type('popUp')),
-                rule(dom('form'), type('popUp')),
-                // rule(type('popUp'), score(oneThirdWidth)),
-                rule(type('popUp'), score(suspiciousClassOrId)),
-                rule(type('popUp'), score(containsForm)),
-                // rule(type('popUp'), score(buttons)),
-                rule(type('popUp').max(), out('popUp'))
-            );
-            return rules;
-        }
-    }
-)
-
-trainees.set(
     // A ruleset that finds the full-screen, content-blocking overlays that
     // often go behind modal popups
-    'overlay',
-    {coeffs: [2, 1, 3, 1, 1],  // 93.8% training-set accuracy with exponentiation-based weights
+    'closeButton',
+    //{coeffs: [1,1,1,1,1],
+    {coeffs: [1,1,1,1],
 
      // viewportSize: {width: 1024, height: 768},
      //
@@ -167,7 +43,9 @@ trainees.set(
         // optimize by rolling it into a class and storing coeffs explicitly in an
         // instance var. [Nope, Spidermonkey does it as efficiently as one could
         // hope, with just a {code, pointer to closure scope} pair.]
-        function ([coeffBig, coeffNearlyOpaque, coeffMonochrome, coeffClassOrId, coeffVisible]) {
+
+        //function ([coeffSmall, coeffSquare, coeffOnClick, coeffClassOrId, coeffVisible]) {
+        function ([coeffSmall, coeffSquare, coeffClassOrId, coeffVisible]) {
             /**
              * We avoid returning full 0 from any rule, because that wipes out the tuner's
              * ability to adjust its impact by raising it to a power. .08 is big enough
@@ -188,50 +66,48 @@ trainees.set(
              * Return whether the passed-in div is the size of the whole viewport/document
              * or nearly so.
              */
-            function big(fnode) {
-                // Compare the size of the fnode to the size of the viewport. So far, spot-
-                // checking shows the overlay is never the size of the whole document, just
-                // the viewport.
+            function small(fnode) {
                 const rect = fnode.element.getBoundingClientRect();
-                const hDifference = Math.abs(rect.height - window.innerHeight);
-                const wDifference = Math.abs(rect.width - window.innerWidth);
-                return trapezoid(hDifference + wDifference, 250, 0) ** coeffBig;  // 250px is getting into "too tall to just be nav or something" territory.
+                const size = Math.abs(rect.height + rect.width);
+                const lowerBound = 25;
+                const upperBound = 150;
+
+                return ((size >= lowerBound && size <= upperBound) ? ONEISH : ZEROISH) ** coeffSmall;
             }
 
-            /**
-             * Return whether the fnode is almost but not entirely opaque.
-             */
-            function nearlyOpaque(fnode) {
-                const style = getComputedStyle(fnode.element);
-                const opacity = parseFloat(style.getPropertyValue('opacity'));
-                let bgColorAlpha = rgbaFromString(style.getPropertyValue('background-color'))[3];
-                if (bgColorAlpha === undefined) {
-                    bgColorAlpha = 1;
-                }
-                const totalOpacity = opacity * bgColorAlpha;
-                let ret;
-                if (totalOpacity === 1) {  // seems to work even though a float
-                    ret = ZEROISH;
-                } else {
-                    ret = trapezoid(totalOpacity, .4, .6);
-                }
-                return ret ** coeffNearlyOpaque;
+            function square(fnode) {
+                const rect = fnode.element.getBoundingClientRect();
+                const lowerBound = 0.5;
+                const upperBound = 1;
+                const smallDim = Math.min(rect.height, rect.width);
+                const largeDim = Math.max(rect.height, rect.width);
+
+                if (smallDim <= 0 || largeDim <= 0)
+                    return ZEROISH ** coeffSquare;
+
+                const ratio = smallDim / largeDim;
+
+                return trapezoid(ratio, lowerBound, upperBound) ** coeffSquare;
             }
 
-            /**
-             * Return whether the fnode's bgcolor is nearly black or white.
-             */
-            function monochrome(fnode) {
-                const rgba = rgbaFromString(getComputedStyle(fnode.element).getPropertyValue('background-color'));
-                return trapezoid(1 - saturation(...rgba), .96, 1) ** coeffMonochrome;
+             function onClick(fnode) {
+                return ((fnode.element.hasAttribute("onclick")) ? ONEISH : ZEROISH) ** coeffOnClick;
             }
+
+            /*
+            function largeZIndex(fnode) {
+                const lowerBound = 0;
+                const upperBound = 1000;
+
+                return trapezoid(fnode.element.style.zIndex, lowerBound, upperBound) ** coeffZIndex;
+            } */
 
             function suspiciousClassOrId(fnode) {
                 const element = fnode.element;
-                const attributeNames = ['class', 'id'];
+                const attributeNames = ['class', 'id', 'button'];
                 let numOccurences = 0;
                 function numberOfSuspiciousSubstrings(value) {
-                    return value.includes('popup') + value.includes('modal') + value.includes('overlay') + value.includes('underlay') + value.includes('backdrop');
+                    return 3*value.includes('close') + value.includes('modal');
                 }
 
                 for (const name of attributeNames) {
@@ -253,15 +129,31 @@ trainees.set(
                 return (-((.3 + ZEROISH) ** (numOccurences + .1685)) + ONEISH) ** coeffClassOrId;
             }
 
-            // *
-            //  * Score hidden things real low.
-            //  *
-            //  * For training, this avoids false failures (and thus gives us more
-            //  * accurate accuracy numbers) since some pages have multiple
-            //  * popups, all but one of which are hidden in our captures.
-            //  * However, for actual use, consider dropping this rule, since
-            //  * deleting popups before they pop up may not be a bad thing.
-             
+            function caselessIncludes(haystack, needle) {
+                return haystack.toLowerCase().includes(needle);
+            }
+
+            function weightedIncludes(haystack, needle, coeff) {
+                return (caselessIncludes(haystack, needle) ? ONEISH : ZEROISH) ** coeff;
+            }
+
+            function hasCloseInClassName(fnode) {
+                return weightedIncludes(fnode.element.className, 'close', coeffHasCloseInClass);
+            }
+
+            function hasCloseInID(fnode) {
+                return weightedIncludes(fnode.element.id, 'close', coeffHasCloseInID);
+            }
+
+            /**
+             * Score hidden things real low.
+             *
+             * For training, this avoids false failures (and thus gives us more
+             * accurate accuracy numbers) since some pages have multiple
+             * popups, all but one of which are hidden in our captures.
+             * However, for actual use, consider dropping this rule, since
+             * deleting popups before they pop up may not be a bad thing.
+             */
             function visible(fnode) {
                 const element = fnode.element;
                 for (const ancestor of ancestors(element)) {
@@ -337,30 +229,21 @@ trainees.set(
             /* The actual ruleset */
 
             const rules = ruleset(
-                rule(dom('div'), type('overlay')),
+                rule(dom('div'), type('closeButton')),
                 // Fuzzy AND is multiplication (at least that's the definition we use,
                 // since Fathom already implements it and it allows participation of
                 // all anded rules.
 
-                // I'm thinking each rule returns a confidence, 0..1, reined in by a sigmoid or trapezoid. 
-                //That seems to fit the features I've collected well. I can probably make up most of those coefficients.
-                // Then we multiply the final results by a coeff each, for weighting. That will cap our total to the sum of the weights. 
-                //We can then scale that sum down to 0..1 if we want, to build upon, by dividing by the product of the weights. 
-                //[Actually, that weighting approach doesn't work, since the weights just get counteracted at the end. 
-                //What we would need is a geometric mean-like approach, where individual rules' output is raised to a power to express its weight. 
-                //Will Fathom's plain linear stuff suffice for now? If we want to keep an intuitive confidence-like meaning for each rule, 
-                //we could have the coeffs be the powers each is raised to. I don't see the necessity of taking the root at the end 
-                //(unless the score is being used as input to some intuitively meaningful threshold later), though we can outside the ruleset 
-                //if we want. Going with a 0..1 confidence-based range means a rule can never boost a score--only add doubt--
-                //but I'm not sure that's a problem. If a rule wants to say "IHNI", it can also return 1 and thus not change the product. 
-                //(Though they'll add 1 to n in the nth-root. Is that a problem?)] 
-                //The optimizer will have to consider fractional coeffs so we can lighten up unduly certain rules.
-                rule(type('overlay'), score(big)),
-                rule(type('overlay'), score(nearlyOpaque)),
-                rule(type('overlay'), score(monochrome)),
-                rule(type('overlay'), score(suspiciousClassOrId)),
-                rule(type('overlay'), score(visible)),
-                rule(type('overlay').max(), out('overlay'))
+                // I'm thinking each rule returns a confidence, 0..1, reined in by a sigmoid or trapezoid. That seems to fit the features I've collected well. I can probably make up most of those coefficients. Then we multiply the final results by a coeff each, for weighting. That will cap our total to the sum of the weights. We can then scale that sum down to 0..1 if we want, to build upon, by dividing by the product of the weights. [Actually, that weighting approach doesn't work, since the weights just get counteracted at the end. What we would need is a geometric mean-like approach, where individual rules' output is raised to a power to express its weight. Will Fathom's plain linear stuff suffice for now? If we want to keep an intuitive confidence-like meaning for each rule, we could have the coeffs be the powers each is raised to. I don't see the necessity of taking the root at the end (unless the score is being used as input to some intuitively meaningful threshold later), though we can outside the ruleset if we want. Going with a 0..1 confidence-based range means a rule can never boost a score--only add doubt--but I'm not sure that's a problem. If a rule wants to say "IHNI", it can also return 1 and thus not change the product. (Though they'll add 1 to n in the nth-root. Is that a problem?)] The optimizer will have to consider fractional coeffs so we can lighten up unduly certain rules.
+                rule(type('closeButton'), score(small)),
+                rule(type('closeButton'), score(square)),
+                //rule(type('closeButton'), score(onClick)),
+                //rule(type('closeButton'), score(zIndex)),
+                rule(type('closeButton'), score(suspiciousClassOrId)),
+                //rule(type('closeButton'), score(hasCloseInClassName)),
+                //rule(type('closeButton'), score(hasCloseInID)),
+                rule(type('closeButton'), score(visible)),
+                rule(type('closeButton').max(), out('closeButton'))
             );
             return rules;
         }
