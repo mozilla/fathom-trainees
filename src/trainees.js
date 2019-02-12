7a24eca4f11bab74f9e535eab5ca5ba1f04f95cf
Patrick Wu <wu.yanxiu27@gmail.com>
2019-02-12 14:57:21 -0500

    Centered is now done

--- src/trainees.js
+++ src/trainees.js
@@ -9,6 +9,158 @@ const trainees = new Map();
 
 
 trainees.set(
+    'popUp',
+    {coeffs: [2,2,1,2,2],  
+
+     rulesetMaker:
+        function ([coeffAncestorArea, coeffForm, coeffClassOrId, coeffCentered, coeffButton]) {
+
+            const ZEROISH = .08;
+            const ONEISH = .9;
+
+
+            function largeAncestor(fnode) {
+                const element = fnode.element;
+                const ancestor = element.parentElement;
+
+                const elementRect = element.getBoundingClientRect();
+                const ancestorRect = ancestor.getBoundingClientRect();
+
+                const elementArea = elementRect.width * elementRect.height;
+                const ancestorArea = ancestorRect.width * ancestorRect.height;
+
+                const windowArea = window.innerWidth * window.innerHeight;
+                const areaDiff = ancestorArea - elementArea;
+
+                return (trapezoid(areaDiff, 0, windowArea)+1) ** coeffAncestorArea;
+            }
+
+            function isCentered(fnode) {
+                const element = fnode.element;
+                const rect = element.getBoundingClientRect();
+
+                const leftDiff = rect.x;
+                const rightDiff = window.innerWidth - rect.x - rect.width;
+
+                const ratio = Math.min(leftDiff, rightDiff)/Math.max(leftDiff, rightDiff);
+
+                const logisticFunction = logisticFuncGenerator(1,0.8, 30, 1);
+                return logisticFunction(ratio) ** coeffCentered;
+            }
+
+            // exponentially decrease each level's score multiplier
+            function containsForm(fnode) {
+                const element = fnode.element;
+                let queue = [element];
+                let formMultiplier = 1;
+
+                for (let i = 1; i < 5; i++){
+                    let nextQueue = [];
+                    while (queue.length > 0){
+                        let e = queue.pop();
+                        nextQueue = nextQueue.concat(Array.from(e.children));
+                        if (e.nodeName === "FORM"){
+                            formMultiplier = formMultiplier * (1+coeffForm/i);
+                        }
+                    }
+                    queue = nextQueue;
+                }
+                return formMultiplier;
+            }
+
+
+            function suspiciousClassOrId(fnode) {
+                const element = fnode.element;
+                const attributeNames = ['class', 'id'];
+                let numOccurences = 0;
+                function numberOfSuspiciousSubstrings(value) {
+                    return value.includes('popup') + value.includes('subscription') + value.includes('newsletter')+ value.includes('modal');
+                }
+
+                for (const name of attributeNames) {
+                    let values = element.getAttribute(name);
+                    if (values) {
+                        if (!Array.isArray(values)) {
+                            values = [values];
+                        }
+                        for (const value of values) {
+                            numOccurences += numberOfSuspiciousSubstrings(value);
+                        }
+                    }
+                }
+                
+                const logisticFunction = logisticFuncGenerator(2,0,coeffClassOrId);
+                return logisticFunction(numOccurences);
+            }
+
+            function buttons(fnode) {
+                // make more specific queryselector
+                let descendants = Array.from(fnode.element.querySelectorAll("*"));
+                let buttonCounter = 0;
+                for (const d of descendants){
+                    if(d.nodeName === "INPUT") {
+                        buttonCounter += 1;
+                    }
+                }
+                return (buttonCounter > 4) ? 1 + 1/buttonCounter : buttonCounter;
+            }
+
+            
+
+            /* Utility procedures */
+
+            /**
+             * Scale a number to the range [ZEROISH, ONEISH].
+             *
+             * For a rising trapezoid, the result is ZEROISH until the input
+             * reaches zeroAt, then increases linearly until oneAt, at which it
+             * becomes ONEISH. To make a falling trapezoid, where the result is
+             * ONEISH to the left and ZEROISH to the right, use a zeroAt greater
+             * than oneAt.
+             */
+            function trapezoid(number, zeroAt, oneAt) {
+                const isRising = zeroAt < oneAt;
+                if (isRising) {
+                    if (number <= zeroAt) {
+                        return ZEROISH;
+                    } else if (number >= oneAt) {
+                        return ONEISH;
+                    }
+                } else {
+                    if (number >= zeroAt) {
+                        return ZEROISH;
+                    } else if (number <= oneAt) {
+                        return ONEISH;
+                    }
+                }
+                const slope = (ONEISH - ZEROISH) / (oneAt - zeroAt);
+                return slope * (number - zeroAt) + ZEROISH;
+            }
+
+            // returns a logistic function. Ideally we should input the growth rate as
+            // a hyperparameter from the optimization
+            function logisticFuncGenerator(maxVal, xMid, growthRate, yInt = 0){
+                return x=> ((maxVal)/(1+Math.exp(-1*growthRate*(x-xMid)))+yInt);
+            }
+
+            /* The actual ruleset */
+
+            const rules = ruleset(
+                rule(dom('div'), type('popUp')),
+                rule(dom('form'), type('popUp')),
+                rule(type('popUp'), score(largeAncestor)),
+                rule(type('popUp'), score(suspiciousClassOrId)),
+                rule(type('popUp'), score(containsForm)),
+                rule(type('popUp'), score(isCentered)),
+                // rule(type('popUp'), score(buttons)),
+                rule(type('popUp').max(), out('popUp'))
+            );
+            return rules;
+        }
+    }
+);
+
+trainees.set(
     // A ruleset that finds the close button associated with a pop-up
     'closeButton',
     {coeffs: [1,1,1,1,1,1,1,1],
@@ -77,7 +229,6 @@ trainees.set(
                 function numberOfSuspiciousSubstrings(value) {
                     return 3*value.includes('close') + value.includes('modal') + value.includes('button');
                 }
-
                 for (const name of attributeNames) {
                     let values = element.getAttribute(name);
                     if (values) {
@@ -89,7 +240,6 @@ trainees.set(
                         }
                     }
                 }
-
                 // Function is Equivalent to 0.9 - 0.38^(0.1685 + numOccurences)
                 return (-((.3 + ZEROISH) ** (numOccurences + .1685)) + ONEISH) ** coeffClassOrId;
             }
@@ -171,15 +321,12 @@ trainees.set(
             function caselessIncludes(haystack, needle) {
                 return haystack.toLowerCase().includes(needle);
             }
-
             function weightedIncludes(haystack, needle, coeff) {
                 return (caselessIncludes(haystack, needle) ? ONEISH : ZEROISH) ** coeff;
             }
-
             function hasCloseInClassName(fnode) {
                 return weightedIncludes(fnode.element.className, 'close', coeffHasCloseInClass);
             }
-
             function hasCloseInID(fnode) {
                 return weightedIncludes(fnode.element.id, 'close', coeffHasCloseInID);
             }
