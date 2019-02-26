333cdf22b07cb4359cbcf544a1bc63e196dff783
ashleysheng <yeer.sheng@gmail.com>
2019-02-26 14:30:01 -0500

    added a rule to fine the highest scoring overlay

--- src/models/popUp.js
+++ src/models/popUp.js
@@ -1,14 +1,150 @@
 import {ruleset, rule, dom, type, score, out} from 'fathom-web';
 import {ancestors} from 'fathom-web/utilsForFrontend';
+import {euclidean} from 'fathom-web/clusters';
 
 const popUpModel = {
-    coeffs: [2,2,1,2,2],  
+    coeffs: [2,2,1,2,2,1],  
 
      rulesetMaker:
-        function ([coeffAncestorArea, coeffForm, coeffClassOrId, coeffCentered, coeffButton]) {
+        function ([coeffAncestorArea, coeffForm, coeffClassOrId, coeffCentered, coeffButton, coeffIsNearOverlay]) {
 
             const ZEROISH = .08;
             const ONEISH = .9;
+            let coeffBigOverlay = 2;
+            let coeffNearlyOpaqueOverlay = 1;
+            let coeffMonochromeOverlay = 3;
+            let coeffClassOrIdOverlay =1;
+            let coeffVisibleOverlay =1;
+
+            function bigOverlay(fnode) {
+                // Compare the size of the fnode to the size of the viewport. So far, spot-
+                // checking shows the overlay is never the size of the whole document, just
+                // the viewport.
+                const rect = fnode.element.getBoundingClientRect();
+                const hDifference = Math.abs(rect.height - window.innerHeight);
+                const wDifference = Math.abs(rect.width - window.innerWidth);
+                return trapezoid(hDifference + wDifference, 250, 0) ** coeffBigOverlay;  // 250px is getting into "too tall to just be nav or something" territory.
+            }
+
+            function nearlyOpaqueOverlay(fnode) {
+                const style = getComputedStyle(fnode.element);
+                const opacity = parseFloat(style.getPropertyValue('opacity'));
+                let bgColorAlpha = rgbaFromString(style.getPropertyValue('background-color'))[3];
+                if (bgColorAlpha === undefined) {
+                    bgColorAlpha = 1;
+                }
+                const totalOpacity = opacity * bgColorAlpha;
+                let ret;
+                if (totalOpacity === 1) {  // seems to work even though a float
+                    ret = ZEROISH;
+                } else {
+                    ret = trapezoid(totalOpacity, .4, .6);
+                }
+                return ret ** coeffNearlyOpaqueOverlay;
+            }           
+
+            function monochromeOverlay(fnode) {
+                const rgba = rgbaFromString(getComputedStyle(fnode.element).getPropertyValue('background-color'));
+                return trapezoid(1 - saturation(...rgba), .96, 1) ** coeffMonochromeOverlay;
+            }    
+
+            function suspiciousClassOrIdOverlay(fnode) {
+                const element = fnode.element;
+                const attributeNames = ['class', 'id'];
+                let numOccurences = 0;
+                function numberOfSuspiciousSubstrings(value) {
+                    // return value.includes('popup') + value.includes('modal');
+                    return value.includes('popup') + value.includes('modal') + value.includes('overlay') + value.includes('underlay') + value.includes('backdrop');
+                }
+
+                for (const name of attributeNames) {
+                    let values = element.getAttribute(name);
+                    if (values) {
+                        if (!Array.isArray(values)) {
+                            values = [values];
+                        }
+                        for (const value of values) {
+                            numOccurences += numberOfSuspiciousSubstrings(value);
+                        }
+                    }
+                }
+
+                // 1 occurrence gets us to about 70% certainty; 2, 90%. It bottoms
+                // out at ZEROISH and tops out at ONEISH.
+                // TODO: Figure out how to derive the magic number .1685 from
+                // ZEROISH and ONEISH.
+                return (-((.3 + ZEROISH) ** (numOccurences + .1685)) + ONEISH) ** coeffClassOrIdOverlay;
+            }       
+            function visibleOverlay(fnode) {
+                const element = fnode.element;
+                for (const ancestor of ancestors(element)) {
+                    const style = getComputedStyle(ancestor);
+                    if (style.getPropertyValue('visibility') === 'hidden' ||
+                        style.getPropertyValue('display') === 'none') {
+                        return ZEROISH ** coeffVisibleOverlay;
+                    }
+                    // Could add opacity and size checks here, but the
+                    // "nearlyOpaque" and "big" rules already deal with opacity
+                    // and size. If they don't do their jobs, maybe repeat
+                    // their work here (so it gets a different coefficient).
+                }
+                return ONEISH ** coeffVisibleOverlay;
+            }
+            /* Utility procedures */
+
+            /**
+             * Return the extracted [r, g, b, a] values from a string like "rgba(0, 5, 255, 0.8)",
+             * and scale them to 0..1. If no alpha is specified, return undefined for it.
+             */
+            function rgbaFromString(str) {
+                const m = str.match(/^rgba?\s*\(\s*(\d+)\s*,\s*(\d+)\s*,\s*(\d+)\s*(?:,\s*(\d+(?:\.\d+)?)\s*)?\)$/i);
+                if (m) {
+                    return [m[1] / 255, m[2] / 255, m[3] / 255, m[4] === undefined ? undefined : parseFloat(m[4])];
+                } else {
+                    throw new Error("Color " + str + " did not match pattern rgb[a](r, g, b[, a]).");
+                }
+            }
+            /**
+             * Return the saturation 0..1 of a color defined by RGB values 0..1.
+             */
+            function saturation(r, g, b) {
+                const cMax = Math.max(r, g, b);
+                const cMin = Math.min(r, g, b);
+                const delta = cMax - cMin;
+                const lightness = (cMax + cMin) / 2;
+                const denom = (1 - (Math.abs(2 * lightness - 1)));
+                // Return 0 if it's black (R, G, and B all 0).
+                return (denom === 0) ? 0 : delta / denom;
+            }
+            function linearScale(number, zeroAt, oneAt) {
+                const isRising = zeroAt < oneAt;
+                if (isRising) {
+                    if (number <= zeroAt) {
+                        return ZEROISH;
+                    }
+                    if (number >= oneAt) {
+                        return ONEISH;
+                    }
+                } else {
+                    if (number >= zeroAt) {
+                        return ZEROISH;
+                    }
+                    if (number <= oneAt) {
+                        return ONEISH;
+                    }
+                }
+                const slope = (ONEISH - ZEROISH) / (oneAt - zeroAt);
+                return slope * (number - zeroAt) + ZEROISH;
+            }
+
+
+            function isNearOverlay(fnode) {
+                const overlayFnode = getHighestScoringOverlay(fnode); 
+                return linearScale(euclidean(fnode, overlayFnode), 1000, 0) ** coeffIsNearOverlay;
+            }
+            function getHighestScoringOverlay(fnode) {
+                return fnode._ruleset.get('overlay')[0]; 
+            }
 
 
             function largeAncestor(fnode) {
@@ -145,7 +281,17 @@ const popUpModel = {
                 rule(type('popUp'), score(containsForm)),
                 rule(type('popUp'), score(isCentered)),
                 // rule(type('popUp'), score(buttons)),
-                rule(type('popUp').max(), out('popUp'))
+                rule(type('popUp'), score(isNearOverlay)),
+
+                rule(type('popUp').max(), out('popUp')),
+
+                rule(dom('div'), type('overlay')),
+                rule(type('overlay'), score(bigOverlay)),
+                rule(type('overlay'), score(nearlyOpaqueOverlay)),
+                rule(type('overlay'), score(monochromeOverlay)),
+                rule(type('overlay'), score(suspiciousClassOrIdOverlay)),
+                rule(type('overlay'), score(visibleOverlay)),
+                rule(type('overlay').max(), out('overlay'))
             );
             return rules;
         }
